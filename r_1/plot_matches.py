4660167f7863e5a6b5610093afbabddd3b28ddc6
ynoplanetashka1 <mrynoplanetashka@gmail.com>
2022-05-25 18:40:27 +0500

    file distribution rework. json data interface reworked. parsers changed to new format.

---
+++ r_1/plot_matches.py
@@ -0,0 +1,26 @@
+import sys
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+
+input_file_name = sys.argv[1]
+
+with open(input_file_name, 'r') as input_file:
+	input_file_text = input_file.read()
+	data = json.loads(input_file_text)
+
+
+	immediate_matches = np.array(data['immediate_tests']['matches'])
+	immediate_errors = np.array(data['immediate_tests']['errors'])
+
+	mean = np.mean(immediate_matches)
+	mean_line = np.full((immediate_matches.size), mean)
+	_, plt1 = plt.subplots()
+	#_, plt2 = plt.subplots()
+	plt1.plot(immediate_matches, 'ro')
+	plt1.plot(mean_line)
+
+	#plt2.hist(immediate_matches, bins = 5, alpha = .5)
+	#plt2.hist(immediate_errors, bins = 5, alpha = .5)
+
+	plt.show()
